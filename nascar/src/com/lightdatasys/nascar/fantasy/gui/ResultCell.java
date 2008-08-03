94483f5079a7c839649368b9f80ad8bbaaef3db7
Matt Light <matt.light@lightdatasys.com>
2008-08-03 21:32:05 +0000

    More result cell fixes...
    git-svn-id: file:///Users/lightster/backup-8.31.2013_20-06-44_litesign/homedir/svn/Workspaces/Dev@39 3baa19a1-490e-4327-b14f-f43bf3c759de

--- nascar/src/com/lightdatasys/nascar/fantasy/gui/ResultCell.java
+++ nascar/src/com/lightdatasys/nascar/fantasy/gui/ResultCell.java
@@ -182,7 +182,7 @@ public class ResultCell extends Cell
 		//g.setFont(g.getFont().deriveFont(36.0f).deriveFont(Font.BOLD));
 		if(result.getLapsDown() != 0)
 			g.setColor(Color.WHITE);
-		else if(result.ledLaps())
+		else if(result.ledLaps() && !result.ledMostLaps())
 			g.setColor(Color.BLACK);
 		else
 			g.setColor(text);
