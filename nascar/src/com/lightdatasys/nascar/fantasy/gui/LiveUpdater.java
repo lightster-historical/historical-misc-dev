a83173787cf031998bc63bd32b23c6452415fd3b
Matt Light <matt.light@lightdatasys.com>
2008-10-05 16:59:56 +0000

    git-svn-id: file:///Users/lightster/backup-8.31.2013_20-06-44_litesign/homedir/svn/Workspaces/Dev@83 3baa19a1-490e-4327-b14f-f43bf3c759de

--- nascar/src/com/lightdatasys/nascar/fantasy/gui/LiveUpdater.java
+++ nascar/src/com/lightdatasys/nascar/fantasy/gui/LiveUpdater.java
@@ -17,11 +17,11 @@ import com.lightdatasys.nascar.fantasy.gui.panel.SortableScroller;
 public class LiveUpdater//extends AppWindow
 	implements Runnable
 {
-	private final static String PROPERTY_FILE = "CampingWorldRV400_2008_2.txt";
-	private final static int RACE_ID = 1077;
+	private final static String PROPERTY_FILE = "AmpEnergy500_2008.txt";
+	private final static int RACE_ID = 1078;
 	
 	
-	private final static boolean ALLOW_UPDATES = false;
+	private final static boolean ALLOW_UPDATES = true;
 	private final static boolean SHOW_FPS = false;
 	
 	
